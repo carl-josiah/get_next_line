d8712d62baebe4bb64f11074a3170df86482a4da
carl-josiah <carl_josiah@yahoo.com>
2024-10-14 14:36:56 +0400

    at uni

--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:05:11 by ccastro           #+#    #+#             */
+/*   Updated: 2024/10/14 13:07:36 by ccastro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+
+#endif
